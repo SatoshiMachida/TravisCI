42ea244fde809d4905ffbd176b62c3d6b9ff9756
SatoshiMachida <jagakon@gmail.com>
2018-04-08 19:16:36 +0900

    Commit 22th

--- tests/hoge_test.py
+++ tests/hoge_test.py
@@ -14,7 +14,7 @@ class HogeTest(unittest.TestCase):
     def test_fuga(self):
         fuga = Fuga()
         self.assertTrue(fuga.index())
-        self.assertEqual(fuga.judge('藤田','喫茶店','いいぜ'),'coffee')
+        self.assertEqual(fuga.judge('伊達市','喫茶店','いいぜ'),'coffee')
         self.assertEqual(fuga.judge('アテン厨','パズドラ','変な'),'ビーフカレー')
         self.assertEqual(fuga.judge('ムーニーマン','15時起き','あれ'),'泡盛')
         self.assertEqual(fuga.judge('チンアナゴ先輩','ドミニオン','聞いてないんだがぁ'),'ブッチャー')
