c60231236d4f9e9d3fbe11962544e08163945364
SatoshiMachida <jagakon@gmail.com>
2018-04-08 19:10:35 +0900

    Commit 21th

--- src/fuga.py
+++ src/fuga.py
@@ -1,10 +1,9 @@
-# coding utf-8
-
 class Fuga:
 
-    FUJITA_POINT=0
-    DATESI_POINT=0
-    KUWABA_POINT=0
+    def __init__(self):
+        self.FUJITA_POINT=0
+        self.DATESI_POINT=0
+        self.KUWABA_POINT=0
     
     def index(self):
         return True
@@ -20,46 +19,42 @@ class Fuga:
         print ('あなたへのおすすめは' + judge(input_test_name, input_test_hobby, input_test_speak) + 'です。')
         
     def checkName(self, name):
-        nonlocal FUJITA_POINT, DATESI_POINT, KUWABA_POINT
         if name in ['Fujita', 'F', '藤田', 'wotsushi', 'Wotsushi', 'W', 'あれ', 'これ', 'それ', '変な', '神', '太陽神', '中本', 'ムーニー', 'ムーニーマン']:
-            FUJITA_POINT+=10
+            self.FUJITA_POINT+=10
         elif name in ['Iwadate', 'I', '岩舘', 'だて', 'date', '伊達市', 'D', 'ヒュゥ', 'シュゥ', 'アァ', '謀略神', '命名神', '第4位', 'マヨネーズLOVE']:
-            DATESI_POINT+=10
+            self.DATESI_POINT+=10
         elif name in ['Kuwabara' ,'K', '桑原', 'KuwabaraK', 'くわ', '煽り魔', 'ヤダヤダヤダヤダ', 'アテン厨', '浦飯交換所', 'ハァア～', 'モンハンプロ']:
-            KUWABA_POINT+=10
+            self.KUWABA_POINT+=10
 
     def checkHobby(self, hobby):
-        nonlocal FUJITA_POINT, DATESI_POINT, KUWABA_POINT
         if hobby in ['水泳', '最適化', 'python', '弁当', '艦これ', '辛いもの', 'ムーニーマン']:
-            FUJITA_POINT+=7
+            self.FUJITA_POINT+=7
         elif hobby in ['喫茶店', 'rails', '残業', '散歩', 'SAO', '禁書目録', '超電磁砲']:
-            DATESI_POINT+=7
+            self.DATESI_POINT+=7
         elif hobby in ['モンハン', 'ネトゲ', '卓球', '15時起き', '使わない自動車']:
-            KUWABA_POINT+=7
+            self.KUWABA_POINT+=7
         elif hobby in ['パズドラ' ,'ドミニオン']:
-            FUJITA_POINT+=5
-            DATESI_POINT+=5
-            KUWABA_POINT+=5            
+            self.FUJITA_POINT+=5
+            self.DATESI_POINT+=5
+            self.KUWABA_POINT+=5            
 
     def checkSpeak(self, speak):
-        nonlocal FUJITA_POINT, DATESI_POINT, KUWABA_POINT
         if speak in ['あれ', 'これ', 'それ', '変な', 'スコヴィル値']:
-            FUJITA_POINT+=4
+            self.FUJITA_POINT+=4
         elif speak in ['ヒュゥ', 'シュゥ', 'アァ', 'フゥ', 'いいぜ']:
-            DATESI_POINT+=4
+            self.DATESI_POINT+=4
         elif speak in ['ヤダヤダヤダヤダ', 'は？', 'それはないっしょ', 'ハァア～', '聞いてないんだがぁ']:
-            KUWABA_POINT+=4
+            self.KUWABA_POINT+=4
 
     def judge(self, name, hobby, speak):
-        nonlocal FUJITA_POINT, DATESI_POINT, KUWABA_POINT
         self.checkName(name)
         self.checkHobby(hobby)
         self.checkSpeak(speak)
-        if FUJITA_POINT >= 15:
+        if self.FUJITA_POINT >= 15:
             return '泡盛'
-        elif DATESI_POINT >= 15:
+        elif self.DATESI_POINT >= 15:
             return 'coffee'
-        elif KUWABA_POINT >= 15:
+        elif self.KUWABA_POINT >= 15:
             return 'ビーフカレー'
         else:
             return 'ブッチャー'
